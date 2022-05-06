2af9d563e67d153769fb058d5620625864dc8658
Ata2400 <104507217+Ata2400@users.noreply.github.com>
2022-05-06 04:07:09 +0300

    Update день_месяц_год.py

--- calendar_Примеры/день_месяц_год.py
+++ calendar_Примеры/день_месяц_год.py
@@ -1,3 +1,11 @@
+
+""" Bu programma arkaly su guni ayy yyly bilip bolyar 
+
+"""
 from datetime import datetime
 Сегодня = datetime.now()
 print(Сегодня.strftime("%d-%m-%Y"))
+
+"""
+day, month, year
+"""
