d9d1a08658a8a3ff436603d5595fb4e97020f1d8
Ata2400 <104507217+Ata2400@users.noreply.github.com>
2022-05-06 04:01:50 +0300

    tazelendi

---
+++ calendar_Примеры/calendar_неделя.py
@@ -0,0 +1,14 @@
+import calendar
+c = calendar.TextCalendar(calendar.MONDAY) # MONDAY islegine gora uytgedip bolyar
+str=c.formatmonth(2022, 5) #2000...2012,2022
+print(str)
+"""
+Понедельник	- Monday
+Вторник	- Tuesday
+Среда -	Wednesday
+Четверг	- Thursday
+Пятница	- Friday
+Суббота	- Saturday
+Воскресенье	- Sunday
+
+"""
--- сегодня какое число/день_месяц_год.py
+++
@@ -1,3 +0,0 @@
-from datetime import datetime
-Сегодня = datetime.now()
-print(Сегодня.strftime("%d-%m-%Y"))
